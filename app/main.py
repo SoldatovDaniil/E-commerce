10504fd378d58f7ae8fed93bee3c934cde6e9b3f
SoldatovDaniil <solddan2014@yandex.ru>
2025-12-02 14:16:13 +0300

    add logs

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from app.routers import categories, products, users, reviews, cart, orders
-
+from app.middleware.log import log_middleware
 
 app = FastAPI(
     title="FastAPI Интернет-магазин"
 )
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
+
+app.middleware("http")(log_middleware)
 
 app.include_router(categories.router)
 app.include_router(products.router)
